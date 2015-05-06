45817d4a74f73d9728547e2a5ed6f2d40451fbfc
Svetlin Penkov <sv.penkov@gmial.com>
2015-05-06 01:17:32 +0100

    Add ROS package files
    Add the package.xml and the CMakeLists.txt files in order to
    create an actual ROS package.

---
+++ src/uArmDraw.cpp
@@ -0,0 +1,15 @@
+/*
+* @Copyright: Copyright[2015]<Svetlin Penkov>
+*      @Date: 2015-05-06
+*     @Email: s.v.penkov@sms.ed.ac.uk
+*      @Desc: This program controls an uArm robot over the serial robot,
+*             in order to draw a picture.
+*/
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main() {
+  return 0;
+}
